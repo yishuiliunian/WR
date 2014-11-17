09ef1d5cf834903a57d76e14db41b1439b157e00
stonedong <yishuiliunian@gmail.com>
2014-11-17 10:55:51 +0800

    Initial Commit

---
+++ WR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WR
+//
+//  Created by stonedong on 14/11/17.
+//  Copyright (c) 2014年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WR
+//
+//  Created by stonedong on 14/11/17.
+//  Copyright (c) 2014年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
